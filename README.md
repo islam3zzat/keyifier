754beb3096cdb0528206a745acb721ecfe9e943a
industrian <industrian@gmail.com>
2024-10-23 22:47:36 +0100

    Changes

--- README.md
+++ README.md
@@ -7,57 +7,59 @@ This repository contains scripts that perform the following actions in commercet
 
 These scripts are intended to prepare Composable Commerce Projects for the new [import](https://docs.commercetools.com/merchant-center/import-data) and [export](https://docs.commercetools.com/merchant-center/export-data) features in the Merchant Center, which use `key` values as identifiers when updating resources.
 
-## Requirements
+## Set up
 
-You require an [API Client](https://docs.commercetools.com/getting-started/create-api-client) with admin rights.
+1. Clone this repository.
+2. Install the dependencies using the command:
+    ```bash
+    npm install
+    ```
+3. Create an [API Client](https://docs.commercetools.com/getting-started/create-api-client) that can manage:
+    - Cart Discounts
+    - Categories
+    - Customer Groups
+    - Discount Codes
+    - Products 
+    - Tax Categories
+4. Download the **Environment Variables (.env)** for this API Client, rename the downloaded file `.env`, and copy it to the cloned repository.
 
-After cloning this repository, create an `.env` file containing the API Client credentials as outlined in [example.env](https://github.com/industrian/keyifier/blob/master/example.env).
-
-Also, remember to install the dependencies using the command:
-
-```bash
-npm install
-```
 
 ## Available scripts
 
-### Query the resources which need keys
+> [!CAUTION]  
+> These scripts apply boilerplate values to resources which do not currently have a `key`. If you have a preferred format or pattern for keys, then modify the code or update the resources seperately.
 
-This will return the number of resources which do not have keys.
+There are two scripts available:
 
-Run the script "Check keys" using the command:
+- Query and apply keys (non-Products)
+- Query and apply keys (Products)
+
+The non-Products script will prompt you to select the resource types to query and update.
+
+Due to the complexity of Products, updating their keys has its own script.
+
+### Query and apply keys (non-Products)
+
+Run the script with the following command:
 
 ```bash
 node queryKeys
 ```
 
-You can select the type of resource to query. If there are fewer than 500, you can also output a list of resources that require `key` values.
+You will be prompted to select the resource types to query.
 
-### Apply keys
+If the queried resource types have resources without keys, you will be prompted to apply keys to them.
 
-> [!CAUTION]  
-> This action applies boilerplate values to resources which do not currently have a `key`. If you have a preferred format or pattern for keys, then modify the code or update them seperately.
+This script applies `key` values using the format:  `{resourceType}_{resourceId}`. For example `categories_9a265100-2997-46d4-affc-2d140efd64ae`. In 99% of cases, this should result in unique keys being applied.
 
-Run the script "Set keys for non-Product resources" using the following command:
+Due to limits of Composable Commerce APIs, you must run this script multiple times if you have over `500` resources of one resource type that require keys.
 
-```bash
-node applyKeys
-```
+### Query and apply keys (Products)
 
-This script applies `key` values using the format: `resourceType_resourceId`. For example `categories_9a265100-2997-46d4-affc-2d140efd64ae`. In most cases, this should result in unique keys being applied.
-
-Due to limits of the Composable Commerce APIs, you will need to run this script multiple times if you have over `500` resources of one resource type that require keys.
-
-
-### Automatically apply keys to non-Product resources
-
-> [!CAUTION]  
-> This action applies boilerplate values to resources which do not currently have a `key`. If you have a preferred format or pattern for keys, then modify the code or update them seperately.
-
-Run the script "Set keys for Products" using the following command:
+You can run the script with the following command:
 
 ```bash
-node updateProductKeys
+node queryProductKeys
 ```
 
 This script applies `key` values to:
@@ -66,6 +68,8 @@ This script applies `key` values to:
 - Product Variants
 - Product Prices
 - Product Assets
+
+This includes `current` and `staged` projections.
 
 This script applies `key` values using the format: `resourceType_resourceId`. For example `product_bd0189b6-a2bc-4f49-8ca9-40919d1bd69e_variant_1_prices_1f70441a-bfdc-4ae3-b07a-3fba8facc0ac`. In most cases, this should result in unique keys being applied.
 
--- queryKeys.js
+++ queryKeys.js
@@ -10,6 +10,32 @@ const CTP_AUTH_URL = process.env.CTP_AUTH_URL;
 const CTP_API_URL = process.env.CTP_API_URL;
 const CTP_SCOPES = process.env.CTP_SCOPES;
 
+// Endpoints to query. These are the GraphQL endpoints, not the HTTP API ones.
+// TODO: Add more endpoints if necessary.
+const endpoints = [
+    "cartDiscounts",
+    "categories",
+    "customerGroups",
+    "discountCodes",
+    "inventoryEntries",
+    "products",
+    "productTypes",
+    "taxCategories",
+];
+
+// Endpoints to make updates to. These are the GraphQL endpoints, not the HTTP API ones.
+// TODO: Add more endpoints if necessary.
+const postEndpoints = [
+    "updateCartDiscount",
+    "updateCategory",
+    "updateCustomerGroup",
+    "updateDiscountCode",
+    "updateInventoryEntry",
+    "updateProduct",
+    "updateProductType",
+    "updateTaxCategory"
+]
+
 // Get the bearer token
 async function getBearerToken() {
     try {
@@ -32,91 +58,117 @@ async function getBearerToken() {
     }
 }
 
-const endpoints = [
-    "cartDiscounts",
-    "categories",
-    "customerGroups",
-    "discountCodes",
-    "inventory",
-    "products",
-    "productTypes",
-    "taxCategories",
-];
-
-async function queryEndpoint(endpoint) {
-
-    const bearerToken = await getBearerToken();
+// Parallel API calls to get the "results" of each PagedQueryResponse
+async function queryResources(arrayOfRequests) {
+    const promises = arrayOfRequests.map(({ url, options }) => fetch(url, options));
     try {
-
-        const response = await fetch(
-            `${CTP_API_URL}/${CTP_PROJECT_KEY}/graphql`,
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${bearerToken}`,
-                    'Content-Type': 'application/json'
-
-                },
-                body: JSON.stringify({
-                    query: `query{ ${endpoint}(limit:500, where:"key is not defined"){ results{ id }  }}`
-                })
-            });
-
-        const data = await response.json();
-        return data.data[endpoint].results.map(result => result.id);
-
+        const responses = await Promise.all(promises);
+        const data = await Promise.all(responses.map(res => res.json()));
+        return data.map(item => item.data);
     } catch (error) {
-        console.error('❌ Error querying endpoint: ', error);
+        console.error('❌  Error querying resources:', error);
     }
 }
 
+// Console input starts
+console.clear();
+
+// Prompt the user to select the resource types.
 const answers = await inquirer.prompt([
     {
-        type: 'list',
-        name: 'selectedEndpoint',
-        message: 'Select an endpoint:',
+        type: 'checkbox',
+        name: 'selectedEndpoints',
+        message: 'Select the resource types to query:',
         choices: endpoints
     }
-])
+]);
 
-let followUp = false;
-let results = [];
+// Contains the results of each query
+let queryResults = [];
+
+// Get the bearer token
+const bearerToken = await getBearerToken();
 
 try {
-    console.log(`You selected: ${answers.selectedEndpoint}`);
-    results = await queryEndpoint(answers.selectedEndpoint)
+    console.log(`You selected: ${answers.selectedEndpoints}`);
 
-    if (results.length >= 500) {
-        console.log(`At least 500 ${answers.selectedEndpoint} require keys.`)
-    }
-    else {
+    // Construct GraphQL calls for each resource type
+    const queryCalls = answers.selectedEndpoints.map(endpoint => ({
+        url: `${CTP_API_URL}/${CTP_PROJECT_KEY}/graphql`,
+        options: {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `query{ ${endpoint}(limit:500, where:"key is not defined"){ results{ id version ${endpoint === "discountCodes" ? `code` : ""} }}}`
+            })
+        }
+    }));
 
-        if (results.length === 0) {
-            console.log(`0 ${answers.selectedEndpoint} require keys.`)
-        }
-        else {
-            console.log(`${results.length} ${answers.selectedEndpoint} require keys.`)
-            followUp = true;
-        }
-    }
+    // Results from each call
+    queryResults = await queryResources(queryCalls)
 }
 catch (error) {
     console.error('An error occurred:', error);
 }
 
-if (followUp) {
-    const outputRequired = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'outputIds',
-            message: `Output ids of ${answers.selectedEndpoint} requiring keys?`,
-            choices: ["Yes", "No"]
-        }
-    ])
+// Put the GraphQL response into a workable array
+const processedResults = queryResults.reduce((acc, item) => {
+    const key = Object.keys(item)[0];
+    acc[key] = item[key].results;
+    return acc;
+}, {});
 
-    if (outputRequired.outputIds === "Yes") {
-        results.forEach(result => {
-            console.log(result)
-        });
+// Boolean array which indicates that keys are needed for resources of that resource type
+let keysAreNeeded = [];
+
+// Loop through the selected resource types and display how many need keys
+console.log("Results: ")
+for (let i = 0; i < answers.selectedEndpoints.length; i++) {
+    console.log(`${processedResults[answers.selectedEndpoints[i]].length} ${answers.selectedEndpoints[i]} need keys.`)
+    if (processedResults[answers.selectedEndpoints[i]].length > 0) { keysAreNeeded.push(endpoints.indexOf(answers.selectedEndpoints[i])) }
+}
+
+console.log("---")
+console.log(keysAreNeeded)
+console.log(processedResults)
+
+// Prompt user to update resources which need keys
+if (keysAreNeeded.length > 0) {
+
+    const choices = keysAreNeeded.map(index => endpoints[index]);
+
+    const updateCalls = await inquirer.prompt([
+        {
+            type: 'select',
+            name: 'selectedEndpoints',
+            message: 'Select the resources to apply keys to:',
+            choices: keysAreNeeded.map(index => endpoints[index])
+        }
+    ]);
+
+    try {
+        console.log(`You selected: ${answers.selectedEndpoints}`);
+
+        const updateCalls = answers.selectedEndpoints.map(endpoint => ({
+            url: `${CTP_API_URL}/${CTP_PROJECT_KEY}/graphql`,
+            options: {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${bearerToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    query: `mutation { updateCategory(id: "", version: 1, actions: [{setKey: {key: "dsds"}}]) { id }}`
+                })
+            }
+        }));
+
+        results = await queryResources(queryCalls)
+    }
+    catch (error) {
+        console.error('An error occurred:', error);
     }
 }
